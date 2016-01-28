78118ac10b39e59d0b450038befa2508f190f48a
John Doe <johndoe@example.com>
2016-01-28 14:51:20 -0600

    Configure routes

--- bookmark-viewer/app/models/bookmark.rb
+++ bookmark-viewer/app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ActiveRecord::Base
-  has_many :saved_bookmarks
+  has_many :saved_bookmarks, dependent: :destroy
   has_many :lists, through: :saved_bookmarks
   has_many :creators, through: :lists, class_name: "User"
 
