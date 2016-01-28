5c05e2834d64c1e2ab235770bf05c513ae9cdef5
John Doe <johndoe@example.com>
2016-01-28 16:35:59 -0600

    Complete register and basic login

--- bookmark-viewer/app/controllers/users_controller.rb
+++ bookmark-viewer/app/controllers/users_controller.rb
@@ -1,18 +1,32 @@
 class UsersController < ApplicationController
 
-  def create
+  def new
+    @user = User.new
+    @errors = @user.errors.full_messages
   end
 
-  def new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to :login
+    else
+      @errors = @user.errors.full_messages
+      render 'users/new'
+    end
   end
 
   def edit
   end
 
   def show
+
   end
 
   def update
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
