de75f836d50fd1807c76363d125c730bd5a52ba6
John Doe <johndoe@example.com>
2016-01-28 14:02:19 -0600

    Complete validations

--- bookmark-viewer/app/models/list.rb
+++ bookmark-viewer/app/models/list.rb
@@ -2,4 +2,7 @@ class List < ActiveRecord::Base
   has_many :saved_bookmarks
   has_many :bookmarks, through: :saved_bookmarks
   belongs_to :creator, class_name: "User"
+
+  validates :creator_id, presence: true
+  validates :name, presence: true
 end
--- bookmark-viewer/app/models/user.rb
+++ bookmark-viewer/app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :lists, foreign_key: :creator_id
   has_many :saved_bookmarks, through: :lists
   has_many :bookmarks, through: :saved_bookmarks
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
