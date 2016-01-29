c03fab18c2c9cbd3237e572ca40a50815e7ef5c3
Kevin Corso & Lars Johnson <pair+corsomk312.larsjx@devbootcamp.com>
2016-01-29 15:27:20 -0600

    Add working seed file & universally renamed file_uploads

--- bookmark-viewer/app/models/user.rb
+++ bookmark-viewer/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :lists, foreign_key: :creator_id
   has_many :saved_bookmarks, through: :lists
   has_many :bookmarks, through: :saved_bookmarks
-  has_one  :file_upload
+  has_one  :upload
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
