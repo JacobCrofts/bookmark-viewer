ce4c223384d2a2b4f91516f7eab7a9bee45e1785
Ronny Ewanek <email": null>
2016-01-30 11:38:39 -0600

    removed id from login

--- bookmark-viewer/config/routes.rb
+++ bookmark-viewer/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/login' => 'sessions#new', as: 'login'
-  post '/login' => 'sessions#create'
+  post '/login' => 'sessions#create', as: 'post_login'
   get '/logout' => 'sessions#destroy', as: 'logout'
 
+  get '/sessions' => 'sessions#show'
   resources :users, :except => [:index, :destroy]
 
   resources :lists, :except => [:index] do
